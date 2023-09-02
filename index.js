47340064d07b96f006f1fcd536c83f5ba2112a36
Dylan Southgate <44190055+DylanSouthgate@users.noreply.github.com>
2023-09-02 12:11:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const ytdl = require('ytdl-core');
 const app = express()
+const ytsr = require('ytsr');
 const port = process.env.PORT || 3000
 
 app.get('/', async(req, res) => {
@@ -29,6 +30,25 @@ app.get('/music', async(req, res) => {
 		console.log(error)
 	}
 })
+
+app.get('/search', async (req, res) =>
+{
+	let string = req.query.string;
+	const searchTerm = req.query.string;
+	try {
+    const filters = await ytsr.getFilters(searchTerm);
+    const filter = filters.get('Type').get('Video');
+    const options = {
+      limit: 10,
+      type: 'audio', // This filters results to audio content
+    };
+
+    const searchResults = await ytsr(filter.url, options);
+    res.end(JSON.stringify(searchResults.items));
+  } catch (error) {
+    console.error('Error searching YouTube:', error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
