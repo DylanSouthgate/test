5e9aa02a8083bed2c9827d55fb3aecc84c70a19c
Dylan Southgate <44190055+DylanSouthgate@users.noreply.github.com>
2023-09-03 00:14:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const ytdl = require('ytdl-core');
 const app = express()
 const ytsr = require('ytsr');
-const port = process.env.PORT || 3000
+const request = require('request');
+const port = process.env.PORT || 4000
 
 app.get('/', async(req, res) => {
   res.send("Hello World!");
 })
+
 
 app.get('/music', async(req, res) => {
   try
@@ -17,9 +19,43 @@ app.get('/music', async(req, res) => {
       let info = await ytdl.getInfo(videoID);
       let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
 			console.log(audioFormats[0].url);
-			const remoteURL = audioFormats[0].url;
+			let remoteURL = await audioFormats[0].url;
 			const data = { message:  remoteURL};
-			res.json(data);
+  const range = req.headers.range;
+
+  // Make a request to the remote URL with the Range header
+  const options = {
+    url: remoteURL,
+    headers: {
+      Range: range || 'bytes=0-', // If no Range header is provided, serve the entire file
+    },
+  };
+
+  request(options)
+    .on('response', (remoteResponse) => {
+      if (remoteResponse.statusCode === 200) {
+        // Full file response
+        res.writeHead(200, {
+          'Content-Length': remoteResponse.headers['content-length'],
+          'Content-Type': 'audio/mpeg', // Set the appropriate content type for your file
+        });
+      } else if (remoteResponse.statusCode === 206) {
+        // Partial content response
+        res.writeHead(206, {
+          'Content-Range': remoteResponse.headers['content-range'],
+          'Accept-Ranges': 'bytes',
+          'Content-Length': remoteResponse.headers['content-length'],
+          'Content-Type': 'audio/mpeg', // Set the appropriate content type for your file
+        });
+      }
+
+      remoteResponse.pipe(res);
+    })
+    .on('error', (error) => {
+      console.error('Error:', error);
+      res.writeHead(500);
+      res.end('Internal Server Error');
+    });
 		}
 		else
 		{
