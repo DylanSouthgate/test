a842389d1e9f66b1dad9795c5c7c8bc1abef8053
Dylan Southgate <44190055+DylanSouthgate@users.noreply.github.com>
2024-03-05 19:05:47 +0200

    Final

--- music/controller.js
+++ music/controller.js
@@ -41,10 +41,12 @@ ytsr(query, options)
 
 module.exports.music1 = async (req, res) => {
   try {
+    console.log("boom");
       if (req.query.link.includes('watch')) {
           let videoID = req.query.link;
           let info = await ytdl.getInfo(videoID);
           let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+          
           console.log(audioFormats[0].url);
           let remoteURL = await audioFormats[0].url;
           const data = { message: remoteURL };
@@ -94,60 +96,16 @@ module.exports.music1 = async (req, res) => {
 module.exports.music = async (req,res) => {
     try
     {
+        console.log("boom");
         if (req.query.link.includes('watch'))
         {
             let videoID = req.query.link;
             let info = await ytdl.getInfo(videoID);
             let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
 			console.log(audioFormats[0].url);
-			let remoteURL = await audioFormats[0].url;
-			const data = { message:  remoteURL};
-            const range = req.headers.range;
+            res.send(audioFormats[0].url);
+        }
+    }catch{
 
-            // Make a request to the remote URL with the Range header
-            const options =
-            {
-                url: remoteURL,
-                headers:
-                {
-                    Range: range || 'bytes=0-', // If no Range header is provided, serve the entire file
-                },
-            };
-            request(options)
-            .on('response', (remoteResponse) =>
-            {
-            if (remoteResponse.statusCode === 200)
-            {
-                // Full file response
-                res.writeHead(200,
-                {
-                'Content-Length': remoteResponse.headers['content-length'],
-                'Content-Type': 'audio/mpeg', // Set the appropriate content type for your file
-                });
-            } else if (remoteResponse.statusCode === 206) {
-        // Partial content response
-        res.writeHead(206, {
-          'Content-Range': remoteResponse.headers['content-range'],
-          'Accept-Ranges': 'bytes',
-          'Content-Length': remoteResponse.headers['content-length'],
-          'Content-Type': 'audio/mpeg', // Set the appropriate content type for your file
-        });
-      }
-
-      remoteResponse.pipe(res);
-    })
-    .on('error', (error) => {
-      console.error('Error:', error);
-      res.writeHead(500);
-      res.end('Internal Server Error');
-    });
-		}
-		else
-		{
-			res.send('boom')
-		}
-	}catch(error)
-	{
-		console.log(error)
-	}
+    }
 }
